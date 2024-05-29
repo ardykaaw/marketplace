3b07bf02cf73123bf25222cfbc7551f075e472ef
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2024-05-29 13:13:02 +0800

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Footer</title>
+    <title>Bagian Footer</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
     <style>
          :root {
